e5f4a934d208a960b9630e265d515f04497d8f84
KD0NKS <andrew.m.fairhurst@gmail.com>
2018-01-04 21:39:42 -0600

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # RazorBootstrapAutocomplete
 Autocomplete for MVC/Razor using jQuery and Bootstrap 3
+
+# Issues
+* Yep!
+
+# Example
+`<div class="dropdown">`
+`    <input type="text" id="autoCompleteTest" />`
+`</div>`
+` `
+`<input type="text" id="autoCompleteSelectedValue" disabled="disabled" />`
+` `
+`$("#autoCompleteTest").autocomplete({`
+`        min: 1`
+`        , source: [{ name: 'test 1', value: 1 }, { name: 'test 2', value: 2 }]`
+`        , displayValue: 'name'`
+`        , selectValue: 'value'`
+`        , target: '#autoCompleteSelectedValue'`
+`        });`
---
+++ autocomplete.js
@@ -0,0 +1,94 @@
+$.fn.autocomplete = function(options) {
+    function guidGenerator() {
+        var S4 = function () {
+            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+        };
+
+        return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+    }
+
+    function IsNullOrEmpty(opt) {
+        return opt === null || opt === undefined || opt === ''
+    }
+
+    if(IsNullOrEmpty(options) || IsNullOrEmpty(options.source)) {
+        throw "Source must be defined for autocomplete element."
+    }
+        
+    if(IsNullOrEmpty(options.min)) {
+        options.min = 1
+    }
+
+    if(IsNullOrEmpty(options.class)) {
+        options.class = 'dropdown-menu'
+    }
+
+    if(!IsNullOrEmpty(options.displayValue) || !IsNullOrEmpty(options.selectValue)) {
+        if(!IsNullOrEmpty(options.displayValue) && IsNullOrEmpty(options.selectValue)) {
+            options.selectValue = options.displayValue
+        } else if(IsNullOrEmpty(options.displayValue) && !IsNullOrEmpty(options.selectValue)) {
+            options.displayValue = options.selectValue
+        }
+    }
+
+    let menuId = 'dd-menu-' + guidGenerator()
+
+    this.attr({
+            'data-toggle': 'dropdown'
+            , 'haspopup': 'true'
+            , 'aria-expanded': 'false'
+            })
+
+    this.after(`<ul id="${menuId}" class="${options.class}"></ul>`)
+    
+    // todo look at bind - https://stackoverflow.com/questions/1948332/detect-all-changes-to-a-input-type-text-immediately-using-jquery
+    // todo look at old val compare - https://stackoverflow.com/questions/1948332/detect-all-changes-to-a-input-type-text-immediately-using-jquery
+    $(this).on('keyup', (e) => {
+        // add exceptions for up, down, enter these will be used for selection instead
+
+        // save old value, if a new one is not selected on blur, set the input display value back to selected display value
+        // set the list back to the selected list item
+
+        let val = e.target.value
+
+        if(!IsNullOrEmpty(val) && val.length >= options.min) {
+            let filtered = []
+
+            if(!IsNullOrEmpty(options.displayValue)) {
+                filtered = options.source.filter(item => item[options.displayValue].indexOf(val) > -1)
+            } else {
+                filtered = options.source.filter(item => item.indexOf(val) > -1)
+            }
+                
+            if(filtered.length > 0) {
+                let menu = ''
+
+                filtered.forEach((item) => {
+                    menu += `<li>`
+                    if(!IsNullOrEmpty(options.displayValue)) {
+                        menu += `<a href="#" data-display="${item[options.displayValue]}" data-value="${item[options.selectValue]}">${item[options.displayValue]}</a>`
+                    } else {
+                        menu += `<a href="#" data-display="${item}" data-value="${item}">${item}</a>`
+                    }
+                    menu += `</li>`
+                })
+
+                $(`#${menuId}`).html(menu)
+
+                $(`#${menuId} li a`).on('click', (selected) => {
+                    this.val(selected.target.getAttribute('data-display'))
+
+                    if(!IsNullOrEmpty(options.target)) {
+                        $(options.target).val(selected.target.getAttribute('data-value'))
+                    }
+
+                    $(`#${menuId}`).html(selected.target.parentElement)
+                })
+            } else {
+                $(`#${menuId}`).html('')
+            }
+        } else {
+            $(`#${menuId}`).html('')
+        }
+    })
+}
