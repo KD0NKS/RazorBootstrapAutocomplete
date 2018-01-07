eb87dd4f9398bc61e22a67dbef3dcdcb720251b5
KD0NKS <andrew.m.fairhurst@gmail.com>
2018-01-06 19:29:24 -0600

    Target is now a function

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ Autocomplete for MVC/Razor using jQuery and Bootstrap 3
 # Issues
 * Yep!
 
+# Todo List
+* look at bindings - https://stackoverflow.com/questions/1948332/detect-all-changes-to-a-input-type-text-immediately-using-jquery
+* look at old val compare - https://stackoverflow.com/questions/1948332/detect-all-changes-to-a-input-type-text-immediately-using-jquery
+* support user defined filters on source array
+* typing delaylook, cancellation of ajax call, etc
+* add exceptions for up, down, enter these will be used for selection instead
+* save old value, if a new one is not selected on blur, set the input display value back to selected display value set the list back to the selected list item
+    * or if input is changed and the target has a value, clear the value of the target
+* source - allow sync and async calls
+
 # Example
 ``` javascript
 <div class="dropdown">
@@ -17,6 +27,29 @@ $("#autoCompleteTest").autocomplete({
         , source: [{ name: 'test 1', value: 1 }, { name: 'test 2', value: 2 }]
         , displayValue: 'name'
         , selectValue: 'value'
-        , target: '#autoCompleteSelectedValue'
+        , target: function() {
+            return $('#autoCompleteSelectedValue')
+        }
         });
+```
+
+
+``` javascript
+$(".autocompleteInput").autocomplete({
+    min: 1
+    , source: function (cb, query) {
+        return $.ajax({
+            url: '@Url.Content("~/")Home/GetFilteredOptions?filter=' + query
+            , method: 'POST'
+            , cache: false
+            , async: false
+            , success: (data) => {
+                cb(data)
+            }
+        })
+    }
+    , target: function(input) {
+        return input.siblings("[id^='autoCompleteSelectedValue']")
+    }
+});
 ```
--- autocomplete.js
+++ autocomplete.js
@@ -1,10 +1,10 @@
 /**
- * source
+ * source - this can be a function or array
  * min
  * class
  * displayValue
  * selectValue
- * target
+ * target - this must be a function.  should this allow strings?
  * ajax calls: must set async to false!
  */
 $.fn.autocomplete = function (options) {
@@ -40,79 +40,82 @@ $.fn.autocomplete = function (options) {
             options.displayValue = options.selectValue
         }
     }
+    
+    return this.each(function () {
+        let menuId = 'dd-menu-' + guidGenerator()
+        let input = $(this)
 
-    // each loop here
-    let menuId = 'dd-menu-' + guidGenerator()
+        input.attr({
+            'data-toggle': 'dropdown'
+            , 'haspopup': 'true'
+            , 'aria-expanded': 'false'
+        })
 
-    this.attr({
-        'data-toggle': 'dropdown'
-        , 'haspopup': 'true'
-        , 'aria-expanded': 'false'
-    })
+        input.after(`<ul id="${menuId}" class="${options.class}"></ul>`)
 
-    this.after(`<ul id="${menuId}" class="${options.class}"></ul>`)
+        // todo look at bind - https://stackoverflow.com/questions/1948332/detect-all-changes-to-a-input-type-text-immediately-using-jquery
+        // todo look at old val compare - https://stackoverflow.com/questions/1948332/detect-all-changes-to-a-input-type-text-immediately-using-jquery
+        // todo support user defined filters on source array
+        // todo typing delaylook
 
-    // todo look at bind - https://stackoverflow.com/questions/1948332/detect-all-changes-to-a-input-type-text-immediately-using-jquery
-    // todo look at old val compare - https://stackoverflow.com/questions/1948332/detect-all-changes-to-a-input-type-text-immediately-using-jquery
-    // todo support user defined filters on source array
-    // todo typing delaylook
-    $(this).on('keyup', (e) => {
-        // add exceptions for up, down, enter these will be used for selection instead
+        input.on('keyup', (e) => {
+            // add exceptions for up, down, enter these will be used for selection instead
 
-        // save old value, if a new one is not selected on blur, set the input display value back to selected display value
-        // set the list back to the selected list item
+            // save old value, if a new one is not selected on blur, set the input display value back to selected display value
+            // set the list back to the selected list item
 
-        let val = e.target.value
+            let val = e.target.value
 
-        if(!IsNullOrEmpty(val) && val.length >= options.min) {
-            let filtered = []
-            let source = []
+            if(!IsNullOrEmpty(val) && val.length >= options.min) {
+                let filtered = []
+                let source = []
 
-            if(typeof options.source === "function") {
-                $(options.source(
-                    function(retVal) {
-                        source = retVal
-                    }
-                    , e.target.value))
-            } else {
-                source = options.source
-            }
-            
-            if(!IsNullOrEmpty(options.displayValue)) {
-                filtered = source.filter(item => item[options.displayValue].toUpperCase().indexOf(val.toUpperCase()) > -1)
-            } else {
-                filtered = source.filter(item => item.toUpperCase().indexOf(val.toUpperCase()) > -1)
-            }
+                if(typeof options.source === "function") {
+                    $(options.source(
+                        function(retVal) {
+                            source = retVal
+                        }
+                        , e.target.value))
+                } else {
+                    source = options.source
+                }
 
-            if(filtered.length > 0) {
-                let menu = ''
+                if(!IsNullOrEmpty(options.displayValue)) {
+                    filtered = source.filter(item => item[options.displayValue].toUpperCase().indexOf(val.toUpperCase()) > -1)
+                } else {
+                    filtered = source.filter(item => item.toUpperCase().indexOf(val.toUpperCase()) > -1)
+                }
 
-                filtered.forEach((item) => {
-                    menu += `<li>`
-                    if(!IsNullOrEmpty(options.displayValue)) {
-                        menu += `<a href="#" data-display="${item[options.displayValue]}" data-value="${item[options.selectValue]}">${item[options.displayValue]}</a>`
-                    } else {
-                        menu += `<a href="#" data-display="${item}" data-value="${item}">${item}</a>`
-                    }
-                    menu += `</li>`
-                })
+                if(filtered.length > 0) {
+                    let menu = ''
 
-                $(`#${menuId}`).html(menu)
+                    filtered.forEach((item) => {
+                        menu += `<li>`
+                        if(!IsNullOrEmpty(options.displayValue)) {
+                            menu += `<a href="#" data-display="${item[options.displayValue]}" data-value="${item[options.selectValue]}">${item[options.displayValue]}</a>`
+                        } else {
+                            menu += `<a href="#" data-display="${item}" data-value="${item}">${item}</a>`
+                        }
+                        menu += `</li>`
+                    })
 
-                $(`#${menuId} li a`).on('click', (selected) => {
-                    this.val(selected.target.getAttribute('data-display'))
+                    $(`#${menuId}`).html(menu)
 
-                    if(!IsNullOrEmpty(options.target)) {
-                        $(options.target).val(selected.target.getAttribute('data-value'))
-                    }
+                    $(`#${menuId} li a`).on('click', (selected) => {
+                        input.val(selected.target.getAttribute('data-display'))
+                        
+                        if(options.target && typeof options.target === "function") {
+                            options.target(input).val(selected.target.getAttribute('data-value'))
+                        }
 
-                    $(`#${menuId}`).html(selected.target.parentElement)
-                })
+                        $(`#${menuId}`).html(selected.target.parentElement)
+                    })
+                } else {
+                    $(`#${menuId}`).html('')
+                }
             } else {
                 $(`#${menuId}`).html('')
             }
-        } else {
-            $(`#${menuId}`).html('')
-        }
+        })
     })
 }
